2eb22c2869cac4fa4c1592433e846468f694c980
CamiloVargas913 <juancamilo.vargas@artemisa.tech>
2022-09-18 16:19:54 -0500

    detalles

--- ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
@@ -4,8 +4,8 @@ namespace ProyectoCiclo3.App.Dominio{
         public int id {get;set;}
         public string nombre {get;set;}            
         public string direccion {get;set;}            
-        public string coord_x {get;set;}            
-        public string coord_y {get;set;}            
+        public float coord_x {get;set;}            
+        public float coord_y {get;set;}            
         public string tipo {get;set;}            
            
     }
